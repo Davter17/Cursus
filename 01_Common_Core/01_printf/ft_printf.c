40ed5159538e355c285b512f167a25929e22105f
Mario Pico Busquier <mpico-bu@c2r17s4.42madrid.com>
2025-01-22 14:01:49 +0100

    ft_printf

--- 01_Common_Core/01_printf/ft_printf.c
+++ 01_Common_Core/01_printf/ft_printf.c
@@ -6,75 +6,56 @@
 /*   By: mpico-bu <mpico-bu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 19:06:05 by mpico-bu          #+#    #+#             */
-/*   Updated: 2025/01/21 16:17:02 by mpico-bu         ###   ########.fr       */
+/*   Updated: 2025/01/22 12:28:20 by mpico-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_print_char(char c)
+int	ft_handle_format(const char *str, va_list args)
 {
-	write(1, &c, 1);
-	return (1);
-}
+	int	len;
 
-int	ft_handle_format(const char **str, va_list args)
-{
-	int	result;
-
-	result = 0;
-	if (**str == 'c')
-		result += ft_print_char((char)va_arg(args, int));
-	else if (**str == 's')
-		result += ft_print_str(va_arg(args, char *));
-	else if (**str == 'p')
-		result += ft_print_adress(va_arg(args, void *));
-	else if (**str == 'i' || **str == 'd')
-		result += ft_print_int(va_arg(args, int));
-	else if (**str == 'u')
-		result += ft_print_unsigned(va_arg(args, unsigned int));
-	else if (**str == 'x')
-		result += ft_print_hex(va_arg(args, int), 0);
-	else if (**str == 'X')
-		result += ft_print_hex(va_arg(args, int), 1);
-	else if (**str == '%')
-		result += ft_print_char('%');
+	len = 0;
+	if (*str == 'c')
+		len += ft_print_char((char)va_arg(args, int));
+	else if (*str == 's')
+		len += ft_print_str(va_arg(args, char *));
+	else if (*str == 'p')
+		len += ft_print_adress(va_arg(args, void *));
+	else if (*str == 'i' || *str == 'd')
+		len += ft_print_int(va_arg(args, int));
+	else if (*str == 'u')
+		len += ft_print_unsigned(va_arg(args, unsigned int));
+	else if (*str == 'x')
+		len += ft_print_hex(va_arg(args, int), 0);
+	else if (*str == 'X')
+		len += ft_print_hex(va_arg(args, int), 1);
+	else if (*str == '%')
+		len += ft_print_char('%');
 	else
-		result += ft_print_char(**str);
-	return (result);
+		len += ft_print_char(*str);
+	return (len);
 }
 
 int	ft_printf(char const *str, ...)
 {
 	va_list	args;
-	int		result;
+	int		len;
 
-	result = 0;
+	len = 0;
 	va_start(args, str);
 	while (*str)
 	{
 		if (*str == '%')
 		{
 			str++;
-			result += ft_handle_format(&str, args);
+			len += ft_handle_format(str, args);
 		}
 		else
-			result += ft_print_char(str[0]);
+			len += ft_print_char(str[0]);
 		str++;
 	}
 	va_end(args);
-	return (result);
+	return (len);
 }
-
-/*
-int	main(void)
-{
-	int result;
-	int *ptr;
-
-	ptr = NULL;
-	result = ft_printf(" %p", NULL);
-	printf("\n%i", result);
-	return (0);
-}
-*/
--- 01_Common_Core/01_printf/ft_printf_utils2.c
+++ 01_Common_Core/01_printf/ft_printf_utils2.c
@@ -1,65 +1,60 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
+/*   ft_printf_utils2.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mpico-bu <mpico-bu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 22:38:58 by mpico-bu          #+#    #+#             */
-/*   Updated: 2025/01/22 12:33:35 by mpico-bu         ###   ########.fr       */
+/*   Updated: 2025/01/22 12:29:03 by mpico-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_print_char(char c)
+int	ft_print_hex(int d, int type)
 {
-	write(1, &c, 1);
-	return (1);
-}
+	unsigned int	num;
+	char			*hex;
+	int				len;
 
-int	ft_print_str(char *s)
-{
-	if (!s)
-	{
-		write(1, "(null)", 6);
-		return (6);
-	}
-	write(1, s, ft_strlen(s));
-	return (ft_strlen(s));
-}
-
-int	ft_print_int(int i)
-{
-	int		len;
-
+	num = (unsigned int)d;
+	if (type == 0)
+		hex = "0123456789abcdef";
+	else
+		hex = "0123456789ABCDEF";
 	len = 0;
-	if (i == -2147483648)
-	{
-		write(1, "-2147483648", 11);
-		return (11);
-	}
-	if (i < 0)
-	{
-		write(1, "-", 1);
-		i = -i;
-		len++;
-	}
-	if (i >= 10)
-		len += ft_print_int(i / 10);
-	write(1, &"0123456789"[i % 10], 1);
+	if (num >= 16)
+		len += ft_print_hex(num / 16, type);
+	write(1, &hex[num % 16], 1);
 	len++;
 	return (len);
 }
 
-int	ft_print_unsigned(unsigned int u)
+int	ft_print_unsigned_hex(uintptr_t u)
 {
 	int	len;
 
 	len = 0;
-	if (u >= 10)
-		len += ft_print_unsigned(u / 10);
-	write(1, &"0123456789"[u % 10], 1);
+	if (u >= 16)
+		len += ft_print_unsigned_hex(u / 16);
+	write(1, &"0123456789abcdef"[u % 16], 1);
 	len++;
+	return (len);
+}
+
+int	ft_print_adress(void *ptr)
+{
+	uintptr_t	address;
+	int			len;
+
+	address = (uintptr_t)ptr;
+	if (!ptr)
+	{
+		write(1, "(nil)", 5);
+		return (5);
+	}
+	write(1, "0x", 2);
+	len = ft_print_unsigned_hex(address) + 2;
 	return (len);
 }
