82d4232947c1f0dd8a3c1d81bbd5ddf894a2f8db
Mario Pico Busquier <mpico-bu@c2r15s2.42madrid.com>
2025-02-17 05:26:45 +0100

    fixing prohibited function

--- 01_Common_Core/02_pipex/main.c
+++ 01_Common_Core/02_pipex/main.c
@@ -6,7 +6,7 @@
 /*   By: mpico-bu <mpico-bu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 19:00:22 by mpico-bu          #+#    #+#             */
-/*   Updated: 2025/02/16 16:38:20 by mpico-bu         ###   ########.fr       */
+/*   Updated: 2025/02/17 04:33:40 by mpico-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,112 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
 
-void	execute_command(const char *cmd, int input_fd, int output_fd)
+#define MAX_ARGS 100
+
+char **parse_command(const char *cmd)
 {
-	pid_t	pid;
-
-	pid = fork();
-	if (pid == -1)
-		exit(1);
-	if (pid == 0)
-	{
-		if (input_fd != STDIN_FILENO)
-		{
-			dup2(input_fd, STDIN_FILENO);
-			close(input_fd);
-		}
-		if (output_fd != STDOUT_FILENO)
-		{
-			dup2(output_fd, STDOUT_FILENO);
-			close(output_fd);
-		}
-		execlp("sh", "sh", "-c", cmd, NULL);
-		exit(127);
-	}
+    char	**args;
+	
+	args = malloc(sizeof(char *) * (MAX_ARGS + 1));
+    if (!args)
+    {
+        perror("malloc");
+        exit(1);
+    }
+    int i = 0;
+    char *cmd_copy = strdup(cmd);
+    if (!cmd_copy)
+    {
+        perror("strdup");
+        exit(1);
+    }
+    char *token = strtok(cmd_copy, " ");
+    while (token && i < MAX_ARGS)
+    {
+        args[i++] = strdup(token);
+        token = strtok(NULL, " ");
+    }
+    args[i] = NULL;
+    free(cmd_copy);
+    return args;
 }
+
+char *find_executable(char *command)
+{
+    if (access(command, X_OK) == 0)
+        return strdup(command);
+    char *path = getenv("PATH");
+    if (!path)
+        return NULL;
+
+    char *path_copy = strdup(path);
+    if (!path_copy)
+        return NULL;
+
+    char *dir = strtok(path_copy, ":");
+    while (dir)
+    {
+        char full_path[1024];
+        snprintf(full_path, sizeof(full_path), "%s/%s", dir, command);
+
+        if (access(full_path, X_OK) == 0)
+        {
+            free(path_copy);
+            return strdup(full_path);
+        }
+        dir = strtok(NULL, ":");
+    }
+    free(path_copy);
+    return NULL;
+}
+
+void execute_command(const char *cmd, int input_fd, int output_fd)
+{
+    pid_t pid = fork();
+    if (pid == -1)
+    {
+        perror("fork");
+        exit(1);
+    }
+    if (pid == 0)
+    {
+        if (input_fd != STDIN_FILENO)
+        {
+            dup2(input_fd, STDIN_FILENO);
+            close(input_fd);
+        }
+        if (output_fd != STDOUT_FILENO)
+        {
+            dup2(output_fd, STDOUT_FILENO);
+            close(output_fd);
+        }
+        char **args = parse_command(cmd);
+        if (!args || !args[0])
+        {
+            perror("parse_command");
+            exit(1);
+        }
+        char *executable = find_executable(args[0]);
+        if (!executable)
+        {
+            fprintf(stderr, "command not found: %s\n", args[0]);
+            exit(127);
+        }
+        execve(executable, args, NULL);
+        perror("execve");
+        exit(127);
+    }
+    else
+		waitpid(pid, NULL, 0);
+}
+
 
 int	open_files(char *input, char *output, int *fd_in, int *fd_out)
 {
@@ -56,18 +138,31 @@ int	open_files(char *input, char *output, int *fd_in, int *fd_out)
 
 pid_t	create_child(const char *cmd, int input_fd, int output_fd, int close_fd)
 {
-	pid_t	pid;
-
+    pid_t	pid;
+	
 	pid = fork();
-	if (pid == 0)
-	{
-		close(close_fd);
-		dup2(input_fd, STDIN_FILENO);
-		dup2(output_fd, STDOUT_FILENO);
-		execlp("sh", "sh", "-c", cmd, NULL);
-		exit(127);
-	}
-	return (pid);
+    if (pid == 0)
+    {
+        close(close_fd);
+        dup2(input_fd, STDIN_FILENO);
+        dup2(output_fd, STDOUT_FILENO);
+        char **args = parse_command(cmd);
+        if (!args || !args[0])
+        {
+            perror("parse_command");
+            exit(1);
+        }
+        char *executable = find_executable(args[0]);
+        if (!executable)
+        {
+            fprintf(stderr, "command not found: %s\n", args[0]);
+            exit(127);
+        }
+        execve(executable, args, NULL);
+        perror("execve");
+        exit(127);
+    }
+    return pid;
 }
 
 int	wait_for_processes(pid_t pid1, pid_t pid2)
